25915ccbd057738c0f6de0ac4d489037fb68acff
Dom <domlwheeler@outlook.com>
2023-05-02 16:24:50 +0100

    "(progress) added a working modal"

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import './App.css';
 import Footer from "./components/Footer";
 import RecentJobs from "./components/RecentJobs";
 import Header from "./components/Header";
-
+import DetailModal from "./components/DetailModal";
 
 const App = () => {
+
     return (
         <>
             <Header/>
             <RecentJobs/>
             <Footer />
+            <DetailModal />
         </>
     )
 }
