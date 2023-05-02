30e638e7ef42586d2a7ab8f8663458b666e5f2cf
James McGraw <james.mcgraw.allen@gmail.com>
2023-05-02 15:05:31 +0100

    (bug) removed issue caused by merge conflict

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -22,6 +22,7 @@ const Header = () => {
                     <li className="nav-item">
                         <a className="nav-link" href="#">Contract</a>
                     </li>
+                </ul>
                 <form className="form-inline my-2 my-lg-0">
                     <button className="btn btn-outline-success my-2 my-sm-0">Search</button>
                 </form>
--- src/components/RecentJobs/index.js
+++ src/components/RecentJobs/index.js
@@ -25,4 +25,3 @@ const RecentJobs = () => {
 }
 
 export default RecentJobs
-
