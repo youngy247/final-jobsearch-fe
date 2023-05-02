7865a857632c1e7dce996121ff5fc49a8664657d
Phildallen <phil_372@live.com>
2023-05-02 14:48:04 +0100

    (progress) populated skills array data into span elements

--- src/components/DetailModal/index.js
+++ src/components/DetailModal/index.js
@@ -8,24 +8,41 @@ const JobDetail = () => {
     // const [logo, setLogo] = useState()
     // const [salary, setSalary] = useState()
     // const [jobType, setJobType] = useState()
-    const [skills, setSkills] = useState({})
+    const [skills, setSkills] = useState([])
 
     const jobID = 300
+    // const skillList = data.skills.map(key => <p>{key}: {skills[key]}</p>)
 
     useEffect(() => {
         fetch('http://localhost:8080/jobs/' + jobID)
             .then (res => res.json())
             .then(data => {
                 setData(data)
+                setSkills(data.skills)
+
             })
-    })
+    }, [])
     return (
         <>
             <p>{data.job_title} - {data.company}</p>
             <h1>{data.job_title} {data.type}</h1>
+            <div>
             <img src={data.logo} />
             <p>{data.company}</p>
+        </div>
+            <h2>Key facts:</h2>
+            <div>
+            <p>Salary: £{data.salary}</p>
+                <p>Type: {data.type}</p>
+                <p>Date posted: {data.posted}</p>
+                {
+                    skills && skills.map((element, i) =>
+                        <span key={i}>{element.skill} </span>
+                    )
+                }
 
+
+        </div>
         </>
     )
 
