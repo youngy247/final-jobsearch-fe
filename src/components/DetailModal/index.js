ef27a60a9e516e58390b7c193ab39f5022ef77f6
Phildallen <phil_372@live.com>
2023-05-02 15:06:15 +0100

    (progress) finished text content

--- src/components/DetailModal/index.js
+++ src/components/DetailModal/index.js
@@ -3,15 +3,9 @@ import {useEffect, useState} from "react";
 const JobDetail = () => {
 
     const [data, setData] = useState({})
-    // const [jobTitle, setJobTitle] = useState()
-    // const [company, setCompany] = useState()
-    // const [logo, setLogo] = useState()
-    // const [salary, setSalary] = useState()
-    // const [jobType, setJobType] = useState()
     const [skills, setSkills] = useState([])
 
-    const jobID = 300
-    // const skillList = data.skills.map(key => <p>{key}: {skills[key]}</p>)
+    const jobID = 412
 
     useEffect(() => {
         fetch('http://localhost:8080/jobs/' + jobID)
@@ -19,33 +13,35 @@ const JobDetail = () => {
             .then(data => {
                 setData(data)
                 setSkills(data.skills)
-
             })
     }, [])
+
     return (
         <>
             <p>{data.job_title} - {data.company}</p>
-            <h1>{data.job_title} {data.type}</h1>
+            <h1>{data.job_title}</h1>
+            <span> {data.type}</span>
+
             <div>
-            <img src={data.logo} />
-            <p>{data.company}</p>
-        </div>
+                <img src={data.logo} />
+                <span>{data.company}</span>
+            </div>
+
             <h2>Key facts:</h2>
+
             <div>
-            <p>Salary: £{data.salary}</p>
-                <p>Type: {data.type}</p>
+                <p>Salary: £{data.salary ?? "Not specified"}</p>
+                <p>Type: {data.type ?? "Not specified"}</p>
                 <p>Date posted: {data.posted}</p>
                 {
                     skills && skills.map((element, i) =>
                         <span key={i}>{element.skill} </span>
                     )
                 }
-
-
-        </div>
+                <h2>Job Description:</h2>
+                <p>{data.job_description}</p>
+            </div>
         </>
     )
-
 }
-
 export default JobDetail
