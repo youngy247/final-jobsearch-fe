43baa36e985b8ca5c038256c5481b96a8d7c868f
Adam Young <adamyoungy678@gmail.com>
2023-05-03 12:03:01 +0100

    complete styling of search bar component

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -2,13 +2,14 @@ import './index.css';
 
 const SearchBar = () => {
     return (
-        <div className="p-sm-2 p-md-5 mx-auto searchBar">
-            <div className="continer w-75 mx-sm-2 mx-md-auto px-sm-2 px-md-5">
-                <p className="fs-1 my-2 mx-">Find your perfect job</p>
-                <div className="row">
+        <div className="container-fluid searchBar p-4 mb-4">
+            <div className="content">
+                <p className="fs-1 my-2 mx-auto">Find your perfect job</p>
+                <div className="col-12">
                     <input className="form-control mx-auto col-12" type="search" placeholder="Search"
                            aria-label="Search"></input>
                 </div>
+
                 <div className="form-check form-check-inline">
                     <input className="form-check-input" type="checkbox" id="inlineCheckbox1" value="FullTime"></input>
                     <label className="form-check-label" htmlFor="inlineCheckbox1">Full time</label>
@@ -21,7 +22,7 @@ const SearchBar = () => {
                     <input className="form-check-input" type="checkbox" id="inlineCheckbox3" value="Contracts"></input>
                     <label className="form-check-label" htmlFor="inlineCheckbox3">Contracts</label>
                 </div>
-                <div className="row col-sm-1 col-md-1">
+                <div className="col-sm-1 col-md-1">
                     <button type="button" className="btn btn-primary">Search</button>
                 </div>
             </div>
