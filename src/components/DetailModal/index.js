fc6fec5d0affda8336119130fa6e4c3012f544c2
Dom <domlwheeler@outlook.com>
2023-05-03 16:01:42 +0100

    (chore) modal formatting completed

--- src/components/DetailModal/index.js
+++ src/components/DetailModal/index.js
@@ -25,49 +25,73 @@ const DetailModal = ({selectedID}) => {
 
     return (
         <>
+            <div class="container">
 
-            <div className="modal fade" id="jobDetails" tabIndex="-1" aria-label="Job Details"
-                 aria-hidden="true">
-                <div className="modal-dialog modal-lg">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <p className="modal-title fs-6">
-                                <strong>{data.job_title} - {data.company}</strong></p>
-                            <button type="button" className="btn-close" data-bs-dismiss="modal"
-                                    aria-label="Close"></button>
-                        </div>
-                        <div className="modal-body">
-                            {loading && <p>Loading...</p>}
-                            {error && <p>Error: {error.message}</p>}
-                            {!loading && !error && (
+                <div className="modal fade" id="jobDetails" tabIndex="-1" aria-labelledby="exampleModalLabel"
+                     aria-hidden="true">
+                    <div className="modal-dialog modal-lg">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <p className="modal-title fs-6" id="exampleModalLabel">
+                                    <strong>{data.job_title} - {data.company}</strong></p>
+                                <button type="button" className="btn-close" data-bs-dismiss="modal"
+                                        aria-label="Close"></button>
+                            </div>
+                            <div className="modal-body">
                                 <div className="container">
 
-                                    <div className="d-flex flex-row bg-primary text-white py-4">
+                                    <div
+                                        className="d-flex flex-sm-column flex-lg-row align-items-start bg-primary text-white py-5 fs-4">
                                         <p className="mx-4"><strong>{data.job_title}</strong></p>
-                                        <p className="bg-success text-white rounded-pill px-1"><strong>{data.type}</strong>
+                                        <p className="bg-success text-white rounded-pill px-3 mx-sm-4 mx-lg-0">
+                                            <strong>{data.type}</strong>
                                         </p>
                                     </div>
-                                    <div>
-                                        {data.logo && <img src={data.logo}/>}
-                                        <p><strong>{data.company}</strong></p>
-                                    </div>
 
-                                    <h2>Key facts:</h2>
+                                    <div className="border border-5 border-light p-3">
 
-                                    <div>
-                                        <p>Salary: £{data.salary ?? "Not specified"}</p>
-                                        <p>Type: {data.type ?? "Not specified"}</p>
-                                        <p>Date posted: {data.posted}</p>
-                                        {
-                                            skills && skills.map((element, i) =>
-                                                <span key={i}>{element.skill} </span>
-                                            )
-                                        }
+                                        <div className="row mx-auto">
+                                            <div className="col-1 p-0"><img src={data.logo} class="mw-100"/></div>
+                                            <p className="col-11 m-0 align-self-center"><strong>{data.company}</strong></p>
+                                        </div>
+
+                                        <h2 className="fs-5 pt-2">Key facts:</h2>
+
+                                        <table className="table table-borderless table-sm">
+
+                                            <thead>
+                                            <tr className="align-top">
+                                                <td scope="col">Salary:</td>
+                                                <td scope="col">Type:</td>
+                                                <td scope="col">Date posted:</td>
+                                                <td scope="col">Skills:</td>
+                                            </tr>
+                                            </thead>
+
+                                            <tbody>
+                                            <tr>
+                                                <td>£{data.salary ? data.salary.toLocaleString() : "Not specified"}</td>
+                                                <td>{data.type ? data.type.replace(' ', '\u00A0') : "Not specified"}</td>
+                                                <td>{new Date(data.posted).toLocaleDateString()}</td>
+                                                <td>
+                                                    {skills && skills.map((element, i) =>
+                                                        <>
+                                                            <span
+                                                                className="rounded bg-info text-white py-0 px-1 fw-bold"
+                                                                key={i}>{element.skill}
+                                                            </span>
+                                                            <span> </span>
+                                                        </>
+                                                    )}
+                                                </td>
+                                            </tr>
+                                            </tbody>
+                                        </table>
                                         <h2>Job Description:</h2>
                                         <p>{data.job_description}</p>
                                     </div>
                                 </div>
-                            )}
+                            </div>
                         </div>
                     </div>
                 </div>
