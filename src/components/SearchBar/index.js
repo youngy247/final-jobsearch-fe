403cd76ef136344664e0cf205725775c89dbc6ad
Adam Young <adamyoungy678@gmail.com>
2023-05-04 15:11:12 +0100

    progress added the checkbox query onto the end of the url on submit

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,16 +1,46 @@
 import './index.css'
 import DisplayJobs from "../DisplayJobs"
-import React from "react"
+import React, { useState } from "react"
 
-const SearchBar = ({setURL, URL, setSelectedID, displayJobs, setDisplayJobs, searched, setSearched}) => {
+const Search = ({ setURL, URL, setSelectedID, displayJobs, setDisplayJobs, searched, setSearched }) => {
+
+    const [fullTimeChecked, setFullTimeChecked] = useState(false);
+    const [partTimeChecked, setPartTimeChecked] = useState(false);
+    const [contractsChecked, setContractsChecked] = useState(false);
 
     const handleSubmit = (e) => {
-        e.preventDefault()
-        setSearched(`search=${URL}`)
+        e.preventDefault();
+
+        let queryString = URL;
+        if (fullTimeChecked) {
+            queryString += "&type[]=Full time";
+        }
+        if (partTimeChecked) {
+            queryString += "&type[]=Part time";
+        }
+        if (contractsChecked) {
+            queryString += "&type[]=Contracts";
+        }
+
+        setSearched(`search=${queryString}`)
+        setURL(`${URL}?${queryString}`)
+
     }
 
     const handleInputChange = (e) => {
-        setURL(e.target.value)
+        setURL(e.target.value);
+    }
+
+    const handleFullTimeChange = (e) => {
+        setFullTimeChecked(e.target.checked);
+    }
+
+    const handlePartTimeChange = (e) => {
+        setPartTimeChecked(e.target.checked);
+    }
+
+    const handleContractsChange = (e) => {
+        setContractsChecked(e.target.checked);
     }
 
     return (
@@ -33,7 +63,9 @@ const SearchBar = ({setURL, URL, setSelectedID, displayJobs, setDisplayJobs, sea
                                 className="form-check-input"
                                 type="checkbox"
                                 id="inlineCheckbox1"
-                                value="FullTime"
+                                value="Full Time"
+                                checked={fullTimeChecked}
+                                onChange={handleFullTimeChange}
                             >
                             </input>
                             <label className="form-check-label" htmlFor="inlineCheckbox1">
@@ -45,7 +77,9 @@ const SearchBar = ({setURL, URL, setSelectedID, displayJobs, setDisplayJobs, sea
                                 className="form-check-input"
                                 type="checkbox"
                                 id="inlineCheckbox2"
-                                value="PartTime"
+                                value="Part Time"
+                                checked={partTimeChecked}
+                                onChange={handlePartTimeChange}
                             ></input>
                             <label className="form-check-label" htmlFor="inlineCheckbox2">
                                 Part time
@@ -57,6 +91,8 @@ const SearchBar = ({setURL, URL, setSelectedID, displayJobs, setDisplayJobs, sea
                                 type="checkbox"
                                 id="inlineCheckbox3"
                                 value="Contracts"
+                                checked={contractsChecked}
+                                onChange={handleContractsChange}
                             ></input>
                             <label className="form-check-label" htmlFor="inlineCheckbox3">
                                 Contracts
@@ -81,4 +117,4 @@ const SearchBar = ({setURL, URL, setSelectedID, displayJobs, setDisplayJobs, sea
     )
 }
 
-export default SearchBar
+export default Search
